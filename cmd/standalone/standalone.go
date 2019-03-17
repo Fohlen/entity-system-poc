d3b3357c76f6835afbba99ac7c34fa2450c6dbad
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-03-17 15:21:52 +0100

    add entity_system skeleton

--- cmd/standalone/standalone.go
+++ cmd/standalone/standalone.go
@@ -3,21 +3,22 @@ package main
 import (
 	"fmt"
 	"github.com/fohlen/entity-system/internal/pkg/attributes"
+	"github.com/fohlen/entity-system/pkg/entity-system"
 	"reflect"
 )
 
 func main()  {
+	var system entity_system.EntitySystem
 	var attribute = attributes.NewAttribute("abc", reflect.String)
 	var instance = attributes.NewInstance(&attribute, "abc")
-	var manager = attributes.GetManager()
-	manager.AddAttribute(&attribute)
-	manager.AddInstance(&instance)
+	system.AttributeManager.AddAttribute(&attribute)
+	system.AttributeManager.AddInstance(&instance)
 
-	for _, attributeUUID := range manager.Types() {
+	for _, attributeUUID := range system.AttributeManager.Types() {
 		fmt.Printf("Attribute UUID: %v\n", attributeUUID)
 	}
 
-	for _, instanceUUID := range manager.Instances() {
+	for _, instanceUUID := range system.AttributeManager.Instances() {
 		fmt.Printf("Instance UUID: %v\n", instanceUUID)
 	}
 }
--- pkg/entity-system/entity-system.go
+++ pkg/entity-system/entity-system.go
@@ -1,1 +1,15 @@
 package entity_system
+
+import (
+	"github.com/fohlen/entity-system/internal/pkg/attributes"
+	"github.com/fohlen/entity-system/internal/pkg/entities"
+)
+
+type EntitySystem struct {
+	AttributeManager attributes.Manager
+	EntityManager entities.Manager
+}
+
+func NewEntitySystem() EntitySystem {
+	return EntitySystem{}
+}
