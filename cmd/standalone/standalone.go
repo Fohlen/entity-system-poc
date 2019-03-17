f2e01b9776e5c94001495ade090de8f5abed98de
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-03-17 16:01:07 +0100

    make standalone run forever

--- cmd/standalone/standalone.go
+++ cmd/standalone/standalone.go
@@ -1,30 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"github.com/fohlen/entity-system/internal/pkg/attributes"
-	"github.com/fohlen/entity-system/internal/pkg/entities"
 	"github.com/fohlen/entity-system/pkg/entity-system"
-	"reflect"
 )
 
 func main()  {
-	var system entity_system.EntitySystem
-	var attribute = attributes.NewAttribute("abc", reflect.String)
-	var instance = attributes.NewInstance(&attribute, "abc")
-	system.AttributeManager.AddAttribute(&attribute)
-	system.AttributeManager.AddInstance(&instance)
-
-	var entity = entities.NewEntity(&attribute)
-	var entityInstance = entities.NewInstance(&entity)
-	system.EntityManager.AddEntity(&entity)
-	system.EntityManager.AddInstance(&entityInstance)
-
-	for _, attributeUUID := range system.EntityManager.Types() {
-		fmt.Printf("Entity type UUID: %v\n", attributeUUID)
-	}
-
-	for _, instanceUUID := range system.EntityManager.Instances() {
-		fmt.Printf("Entity instance UUID: %v\n", instanceUUID)
-	}
+	var system = entity_system.NewEntitySystem()
+	system.Init()
+	system.Run()
 }
--- pkg/entity-system/entity-system.go
+++ pkg/entity-system/entity-system.go
@@ -1,8 +1,11 @@
 package entity_system
 
 import (
+	"fmt"
 	"github.com/fohlen/entity-system/internal/pkg/attributes"
 	"github.com/fohlen/entity-system/internal/pkg/entities"
+	"reflect"
+	"time"
 )
 
 type EntitySystem struct {
@@ -12,4 +15,34 @@ type EntitySystem struct {
 
 func NewEntitySystem() EntitySystem {
 	return EntitySystem{}
+}
+
+func (entitySystem *EntitySystem) Init() {
+	var attribute = attributes.NewAttribute("abc", reflect.String)
+	var instance = attributes.NewInstance(&attribute, "abc")
+	entitySystem.AttributeManager.AddAttribute(&attribute)
+	entitySystem.AttributeManager.AddInstance(&instance)
+
+	var entity = entities.NewEntity(&attribute)
+	var entityInstance = entities.NewInstance(&entity)
+	var entityInstance2 = entities.NewInstance(&entity)
+	entitySystem.EntityManager.AddEntity(&entity)
+	entitySystem.EntityManager.AddInstance(&entityInstance)
+	entitySystem.EntityManager.AddInstance(&entityInstance2)
+
+	for _, attributeUUID := range entitySystem.EntityManager.Types() {
+		fmt.Printf("Entity type UUID: %v\n", attributeUUID)
+	}
+
+	for _, instanceUUID := range entitySystem.EntityManager.Instances() {
+		fmt.Printf("Entity instance UUID: %v\n", instanceUUID)
+	}
+}
+
+func (entitySystem *EntitySystem) Run() {
+	var startTime = time.Now()
+	for {
+		time.Sleep(2 * time.Second)
+		fmt.Printf("Uptime: %v\n", time.Now().Sub(startTime))
+	}
 }
